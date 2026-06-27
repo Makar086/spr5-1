5ed74c06f0b8c5e0d270e18f019cfaec90186e7f
Anton Makarov <makar086@yandex.ru>
2026-06-28 00:19:25 +0500

    сделал все по инструкции

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,32 @@
-package actioninfo
+﻿package actioninfo
+import(
+	"log"
+	
+	
+		
+)
 
 type DataParser interface {
 	// TODO: добавить методы
+
+	Parse(string) (error)  
+	
+	ActionInfo() (string, error)
+
 }
 
 func Info(dataset []string, dp DataParser) {
 	// TODO: реализовать функцию
+for q := range dataset{
+er:= dp.Parse(dataset[q])
+	if er !=nil{
+	log.Println("Ошибка парсинга для элемента", er)
+			continue
+	}
+ dp.ActionInfo()
+if er !=nil{
+	log.Println("Ошибка парсинга для элемента", er)
+			}
+return 
+}
 }
